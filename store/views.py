f12c23dd89685e47bcc8b96ae0c5f40424f5fd07
Sergey <serjkasmail@gmail.com>
2020-12-18 03:13:03 -0800

    создал сериалайзер, добавил в админку отображение, создал тест по APITestCase

--- store/views.py
+++ store/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from store.models import Book
+from store.serializers import BooksSerializer
+
+
+class BookViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BooksSerializer
